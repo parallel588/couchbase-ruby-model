518f7b4df8cf460e738fc60193b204a1da2adbe2
Maxim Pechnikov <parallel588@gmail.com>
2013-11-23 17:37:16 +0400

    added attributes to template model

--- lib/rails/generators/couchbase/model/model_generator.rb
+++ lib/rails/generators/couchbase/model/model_generator.rb
@@ -2,16 +2,28 @@ require 'rails/generators/couchbase_generator'
 
 module Couchbase
   module Generators
-    class ModelGenerator < Rails::Generators::Base
+    class ModelGenerator < Rails::Generators::NamedBase
+      desc 'Creates a couchbase model'
+
+      argument :name, :type => :string
+      argument :attributes, :type => :array, :default => [], :banner => "field field"
+
+      check_class_collision
+
+      def self.source_root
+        @_couchbase_source_root ||= File.expand_path('../templates', __FILE__)
+      end
 
       def create_model_file
-        template 'model.rb', File.join('app/models', class_path, "#{file_name}.rb")
+        template 'model.rb', File.join('app', 'models', class_path, "#{file_name}.rb")
       end
 
       def create_module_file
         return if class_path.empty?
-        template 'module.rb', File.join('app/models', "#{class_path.join('/')}.rb") if behavior == :invoke
+        template 'module.rb', File.join('app', 'models', "#{class_path.join('/')}.rb") if behavior == :invoke
       end
+
+      hook_for :test_framework
 
       protected
 
--- lib/rails/generators/couchbase/model/templates/model.rb
+++ lib/rails/generators/couchbase/model/templates/model.rb
@@ -1,2 +1,8 @@
 class <%= class_name %> < <%= parent_class_name.classify %>
+
+<%- attributes.each do |attribute| -%>
+  attribute :<%= attribute.name %>
+<% end -%>
+
+
 end
